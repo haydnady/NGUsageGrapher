3ed83c63bec1d6303190a91c6172f8e93fc37f6f
Haydn <haydnady@gmail.com>
2020-07-16 23:46:49 -0400

    Updated graph and  labels

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import plotly.graph_objects as go
+import datetime
 
 
 # Insert file name/path
@@ -9,20 +10,39 @@ data = pd.read_csv(FILENAME)
 
 dates = list(data["ReadDate & Days"])
 # Spliting date data into sections i.e. "2/2/2020" becomes ["2", "2", "2020"].
-# Then adding each section back into dataframe, after creating column in dataframe.
+# Then adding each section back into dataframe, after creating columns in dataframe.
 dateSplit = []
-data.insert(0, "Month", 0)
-data.insert(1, "Day", 0)
-data.insert(2, "Year", 0)
-data.insert(3, "Total Days", 0)
+data.insert(0, "Month Names", 0)
+data.insert(1, "Months", 0)
+data.insert(2, "Days", 0)
+data.insert(3, "Years", 0)
+data.insert(4, "Total of Days", 0)
 
 for i, row in data.iterrows():
     dateSplit = row["ReadDate & Days"].replace(" & ", "/").replace(" Days", "").split("/")
 
-    data["Month"][i] = dateSplit[0]
-    data["Day"][i] = dateSplit[1]
-    data["Year"][i] = dateSplit[2]
-    data["Total Days"][i] = dateSplit[3]
+    data["Month Names"][i] = datetime.date(1900, int(dateSplit[0]), 1).strftime('%B')
+    data["Months"][i] = dateSplit[0]
+    data["Days"][i] = dateSplit[1]
+    data["Years"][i] = dateSplit[2]
+    data["Total of Days"][i] = dateSplit[3]
+
+data.sort_values(by=["Years", "Months"], inplace=True, ascending=True)
+
+#  Graphing using plotly
+fig = go.Figure(data=[
+    go.Bar(name='year1', x=data["Month Names"], y=data["Total kWh"]["2018"]),
+    go.Bar(name='Year2', x=data["Month Names"], y=data["Total kWh"]),
+    go.Bar(name='year3', x=data["Month Names"], y=data["Total kWh"])
+    ])
 
 
-print(data) 
+# Layout
+fig.update_layout(barmode='group',
+                  title="Nationalgrid Usage Report",
+                  yaxis_title="kWh",
+                  xaxis_tickangle=-45)
+
+fig.show()
+
+
