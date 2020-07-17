00ec607856e403a286c554c76565f1255829ca52
Haydn <haydnady@gmail.com>
2020-07-16 22:13:50 -0400

    Split date data added to dataframe

--- main.py
+++ main.py
@@ -4,17 +4,25 @@ import plotly.graph_objects as go
 
 # Insert file name/path
 FILENAME = "FILENAME"
+
 data = pd.read_csv(FILENAME)
 
 dates = list(data["ReadDate & Days"])
+# Spliting date data into sections i.e. "2/2/2020" becomes ["2", "2", "2020"].
+# Then adding each section back into dataframe, after creating column in dataframe.
+dateSplit = []
+data.insert(0, "Month", 0)
+data.insert(1, "Day", 0)
+data.insert(2, "Year", 0)
+data.insert(3, "Total Days", 0)
 
-#  Graphing using 
-fig = go.Figure(data=[
-    go.Bar(name='Nationalgrid', x=dates, y=list(data["Total kWh"])),
-    # go.Bar(name='Price', x=months, y=[data["Total kWh"]])
-])
+for i, row in data.iterrows():
+    dateSplit = row["ReadDate & Days"].replace(" & ", "/").replace(" Days", "").split("/")
 
-# fig.update_layout(barmode='group') # Change the bar mode
-fig.show()
+    data["Month"][i] = dateSplit[0]
+    data["Day"][i] = dateSplit[1]
+    data["Year"][i] = dateSplit[2]
+    data["Total Days"][i] = dateSplit[3]
 
-print(list(data["Total kWh"]))
+
+print(data) 
